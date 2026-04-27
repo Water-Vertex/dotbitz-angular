39306bfc156edb561d7a741ab55f641d578eab0e
Fizza Batool <fizza.batool@watervertex.com>
2026-04-27 15:48:39 +0500

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,18 @@
 export const environment = {
   production: true,
   // local API URLs'
-  // AdminApiUrl: 'http://localhost:8000/api/admin',
-  // GuardianApiUrl: 'http://localhost:8000/api/guardian',
-  // StudentApiUrl: 'http://localhost:8000/api/student',
-  // InstructorApiUrl: 'http://localhost:8000/api/instructor',
-  // GuestApiUrl: 'http://localhost:8000/api/guest',
+  AdminApiUrl: 'http://localhost:8000/api/admin',
+  GuardianApiUrl: 'http://localhost:8000/api/guardian',
+  StudentApiUrl: 'http://localhost:8000/api/student',
+  InstructorApiUrl: 'http://localhost:8000/api/instructor',
+  GuestApiUrl: 'http://localhost:8000/api/guest',
   stripePublicKey: 'pk_test_51T6uTDRzKUGRV6Plu9rV6yg1tut179Wy49ToZbjNMAFHBbDrx0LokYnsz592jArhnpDmFJY06DgYAY1jPdsbXSWb00xQ3HKKQd',
 
   // live API URLs
-  AdminApiUrl: 'https://dotbitz.com/api/admin',
-  GuardianApiUrl: 'https://dotbitz.com/api/guardian',
-  StudentApiUrl: 'https://dotbitz.com/api/student',
-  InstructorApiUrl: 'https://dotbitz.com/api/instructor',
-  GuestApiUrl: 'https://dotbitz.com/api/guest',
+  // AdminApiUrl: 'https://dotbitz.com/api/admin',
+  // GuardianApiUrl: 'https://dotbitz.com/api/guardian',
+  // StudentApiUrl: 'https://dotbitz.com/api/student',
+  // InstructorApiUrl: 'https://dotbitz.com/api/instructor',
+  // GuestApiUrl: 'https://dotbitz.com/api/guest',
   // stripePublicKey: 'pk_test_51T6uTDRzKUGRV6Plu9rV6yg1tut179Wy49ToZbjNMAFHBbDrx0LokYnsz592jArhnpDmFJY06DgYAY1jPdsbXSWb00xQ3HKKQd'
 };
